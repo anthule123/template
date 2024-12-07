825c515896776de4bf7d060d1d981f1bd4cee559
an-yellow <anthule2000@gmail.com>
2024-12-07 23:18:48 +0700

    sua

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // Thêm dòng này
-  images: {
-    unoptimized: true, // Thêm dòng này nếu dùng Next/Image
-  },
-  basePath: "/template", // Thêm basePath nếu deploy lên GitHub Pages
 };
 
 export default nextConfig;
