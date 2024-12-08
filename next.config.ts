ba5b4df1bda3f75056a5c9e2d452e97029e1936c
an-yellow <anthule2000@gmail.com>
2024-12-08 08:16:40 +0700

    z

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  //output: "export", // Thêm dòng này
+  output: "export", // Thêm dòng này
   // Thêm cấu hình để generate các dynamic routes
   generateStaticParams: async () => {
     // Đây là nơi bạn định nghĩa tất cả các dynamic paths
